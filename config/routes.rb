b57187264884a8657f51922a20e900806e1e02ab
Matt Fair <matt.j.fair@gmail.com>
2015-10-29 17:30:55 -0400

    Normal Mode Done

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
 
-   root 'welcome#index', :as => :welcome_page
+   root 'welcome#index'
 
    get '/posts/:id' => 'welcome#posts', :as => :posts
 
