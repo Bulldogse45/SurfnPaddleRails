13a5f9df69317b87cf98675936aa317ff25ffcde
Matt Fair <matt.j.fair@gmail.com>
2015-11-03 17:30:48 -0500

    Authors db created

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151103195828) do
+ActiveRecord::Schema.define(version: 20151103222239) do
+
+  create_table "authors", force: :cascade do |t|
+    t.string   "name"
+    t.text     "bio"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
