14ae857c44f5a7e8216d82ffa8e9847504e0d332
Matt Fair <matt.j.fair@gmail.com>
2015-11-04 00:06:16 -0500

    Fixed everything I broke trying to test

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,10 +1,10 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  test "the truth" do
-    assert true
-    true
-  end
+  # test "the truth" do
+  #   assert true
+  #   true
+  # end
   # test "Posts authors associated correctly" do
   #   assert true
   #   post=Post.create(author_id:1, title:"The Adventures of Huckleberry Finn", body:"This is the song that never ends.  It goes on and on my friend. Some people started singing it not knowing what it was, and they'll continue singing it forever just because.")
